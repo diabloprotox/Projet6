fbe447d9adc4af8700c038338eb3f68e333deaf5
Josias <jsqtrivera@gmail.com>
2021-12-08 00:54:54 +0100

    warning

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    try{
+    try {
         const token = req.headers.autorization.split(' ')[1];
-        const decodedToken = jwt.verify(token,'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         req.auth = { userId: userId };
-        if(req.body.userId && req.body.userId !== userId) {
-            throw 'Invalid user ID';
-        }else{
+        if (req.body.userId && req.body.userId !== userId) {
+            throw new Error('Invalid user ID');
+        } else {
             next();
         }
-    }catch{
+    } catch {
         res.status(401).json({
-            error : new Error('Invalid request!')
+            error: new Error('Invalid request!')
         });
     }
 };
